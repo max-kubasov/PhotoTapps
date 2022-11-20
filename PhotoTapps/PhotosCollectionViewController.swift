90d928a7ddf5e3393ab180411d6c0bcaafbd0892
max-kubasov <kubasov87@gmail.com>
2022-11-20 18:14:25 +0200

    Flow layout advanced setup

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    let itemsPerRow: CGFloat = 2
+    let sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +32,8 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-    
         cell.backgroundColor = .black
     
         return cell
@@ -39,27 +42,27 @@ class PhotosCollectionViewController: UICollectionViewController {
 }
 
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 2
-        let paddingWidth = 20 * (itemsPerRow + 1)
+
+        let paddingWidth = sectionInsets.left * (itemsPerRow + 1)
         let avaiblableWidth = collectionView.frame.width - paddingWidth
         let widthPerItem = avaiblableWidth / itemsPerRow
-        
-        
+
+
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return sectionInsets
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInsets.left
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return sectionInsets.left
     }
-    
+
 }
